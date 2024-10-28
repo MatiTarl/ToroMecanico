9a393ffb9311c5eed95d7d5fbb96f61215b016fb
MatiTarl <Matiasvalentintari123@gmail.com>
2024-10-28 16:32:37 -0300

    agregado de componente navbar y lista

---
+++ app/components/Navbar.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+export default function Navbar() {
+  return (
+    <section className="container flex p-3 justify-center w-full">
+      <h1 className="font-serif text-3xl w-[70%]">TEXAS BULL</h1>
+      <div className="flex justify-center w-[70%]">
+        <li className="flex flex-row space-x-10">
+          <ul>1</ul>
+          <ul>2</ul>
+          <ul>3</ul>
+          <ul>4</ul>
+        </li>
+      </div>
+    </section>
+  );
+}
--- app/productos/layout.js
+++ app/productos/layout.js
@@ -1,3 +1,7 @@
+
+
+
+
 export default function RootLayout({ children }) {
     return (
       <html lang="en">
