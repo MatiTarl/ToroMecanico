bc1e75b7c54b99b24742675703c0b7d87c15abe4
MatiTarl <Matiastari@outlook.com.ar>
2024-10-29 18:55:40 -0300

    Agregado de video 4k para fondo en public, se agregaron titulos, subtitulos y botones con estilos basicos

--- app/components/Navbar.jsx
+++ app/components/Navbar.jsx
@@ -2,24 +2,21 @@ import React from "react";
 
 export default function Navbar() {
   return (
-    <section className="container flex p-3 justify-around w-full pt-10 pl-5 items-center">
-      <h1 className="text-3xl w-[70%]">TEXAS BULL</h1>
-      <div className="flex justify-center w-[70%]">
-        <li className="flex flex-row space-x-40">
-          <ul className="">
-            <a href="">INICIO</a>
-          </ul>
-          <ul className="">
-            <a href="">PRODUCTOS</a>
-          </ul>
-          <ul className="">
-            <a href="">NOSOTROS</a>
-          </ul>
-          <ul className="">
-            <a href="">CONTACTANOS</a>
-          </ul>
+    <nav className="container flex justify-around w-full items-center">
+      <ul className="flex flex-row space-x-20 font-roboto">
+        <li className="">
+          <a href="">INICIO</a>
         </li>
-      </div>
-    </section>
+        <li className="">
+          <a href="">PRODUCTOS</a>
+        </li>
+        <li className="">
+          <a href="">NOSOTROS</a>
+        </li>
+        <li className="">
+          <a href="">CONTACTANOS</a>
+        </li>
+      </ul>
+    </nav>
   );
 }
--- app/page.js
+++ app/page.js
@@ -2,8 +2,31 @@ import Navbar from "./components/Navbar";
 
 export default function Home() {
   return (
-    <div className="w-full justify-center flex">
-      <Navbar/>
+    <div>
+      <header className="w-full justify-between flex items-center px-10 py-6 top-0 fixed z-40">
+        <div className="w-1/2 justify-center flex">
+          <h1 className="text-3xl">TEXAS BULL</h1>
+        </div>
+        <Navbar />
+      </header>
+      <section className="bg-black h-screen w-screen text-center relative overflow-hidden">
+        <div className="relative z-30 h-full flex flex-col font-roboto">
+          <header className=" pt-52">
+            {/* <h1 className="text-start">TEXAS BULL</h1> */}
+            <h2 className=" text-5xl font-medium">Siente la adrenalina de los toros mecánicos más reales</h2>
+            <h5 className="text-xl">Experiencia y seguridad en cada desafío</h5>
+          </header>
+
+          <footer className="flex-grow flex flex-col justify-end pb-32">
+            <div className="">
+              <a href="#" className="text-xl border-2 rounded font-medium px-12 py-2 inline-block">Descubre más</a>
+            </div>
+          </footer>
+        </div>
+        <div className="absolute top-0 bottom-0">
+          <video autoPlay muted loop src="/hombreEnToro4k.webm" className="object-cover object-center h-full 2xl:h-auto"></video>
+        </div>
+      </section>
     </div>
   );
 }
