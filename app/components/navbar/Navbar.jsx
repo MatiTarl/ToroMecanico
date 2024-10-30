e5e679066af74f52810a05b889d50a35ab85f4c2
MatiTarl <Matiasvalentintari123@gmail.com>
2024-10-30 12:55:07 -0300

    agregado de colores en diseño de navbar, y en el header tambien

--- app/components/navbar/Navbar.jsx
+++ app/components/navbar/Navbar.jsx
@@ -1,9 +1,10 @@
 import React from "react";
+import "./module.css";
 
 export default function Navbar() {
   return (
     <nav className="container flex justify-around w-full items-center">
-      <ul className="flex flex-row space-x-20 font-roboto">
+      <ul className="flex flex-row space-x-20 text-red-700 font-black text-shadow">
         <li className="">
           <a href="">INICIO</a>
         </li>
--- app/page.js
+++ app/page.js
@@ -1,19 +1,18 @@
-import Navbar from "./components/Navbar";
+import Navbar from "./components/navbar/Navbar";
 
 export default function Home() {
   return (
     <div>
       <header className="w-full justify-between flex items-center px-10 py-6 top-0 fixed z-40">
         <div className="w-1/2 justify-center flex">
-          <h1 className="text-3xl">TEXAS BULL</h1>
+          <h1 className="text-5xl ratio">TEXAS BULL</h1>
         </div>
         <Navbar />
       </header>
-      <section className="bg-black h-screen w-screen text-center relative overflow-hidden">
-        <div className="relative z-30 h-full flex flex-col font-roboto">
+      <section className="bg-black h-screen w-screen text-center relative overflow-hidden ">
+        <div className="relative z-30 h-full flex flex-col font-sans">
           <header className=" pt-52">
-            {/* <h1 className="text-start">TEXAS BULL</h1> */}
-            <h2 className=" text-5xl font-medium">Siente la adrenalina de los toros mecánicos más reales</h2>
+            <h2 className=" text-5xl font-semibold bg-gradient-to-r from-red-700 via-white to-red-700 text-transparent bg-clip-text">Subite a la experiencia más intensa</h2>
             <h5 className="text-xl">Experiencia y seguridad en cada desafío</h5>
           </header>
 
@@ -24,7 +23,7 @@ export default function Home() {
           </footer>
         </div>
         <div className="absolute top-0 bottom-0 w-full h-full">
-          <video autoPlay muted loop src="/hombreEnToro4k.webm" className="object-cover object-center h-full w-full"></video>
+          <video autoPlay muted loop src="/hombreEnToro4k.webm" className="object-cover object-center h-full w-full opacity-70"></video>
         </div>
       </section>
     </div>
