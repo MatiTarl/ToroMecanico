e2cba54d9e439889a78ec3044c7f285a8b81ba33
MatiTarl <Matiasvalentintari123@gmail.com>
2024-11-15 20:46:38 -0300

    Ultimo push

--- app/components/footer/Footer.jsx
+++ app/components/footer/Footer.jsx
@@ -5,8 +5,9 @@ import Image from 'next/image';
 export default function Footer() {
   return (
     <footer className='flex flex-row justify-between bg-rojoprincipal bg-opacity-70 w-screen h-32'>
-        <div className=' flex w-1/2 justify-center'>
+        <div className=' flex w-1/2 justify-center items-center'>
             <Image src={TexasIcon} width={Infinity} height={Infinity} className='h-14 w-10'/>
+            <h1></h1>
         </div>
         <div className=''>
         </div>
