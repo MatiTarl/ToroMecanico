c1d8574eedace6d5f4acdb54fa4c8cf7a8089728
MatiTarl <Matiastari@outlook.com.ar>
2024-11-28 20:48:59 -0300

    agregado de diseño responsive en apartado rpoductos y correciones de navbar

--- app/components/ProductosSection/ProductosSection.jsx
+++ app/components/ProductosSection/ProductosSection.jsx
@@ -7,26 +7,30 @@ const ProductosSection = () => {
 
   return (
     <main className="h-full w-full py-5 px-1 justify-center items-center flex flex-col">
-      <h1 className="text ratio text-xl text-rojosecundario ">Nuestros Productos</h1>
-
-      <div className="grid grid-cols-2 gap-2 justify-center pt-10">
+      <div className="grid grid-cols-1 gap-4 justify-center pt-10 w-full">
         {productosToMap.map((producto, key) => {
           return (
-            <a href={``}  key={key}>
-              <div className="flex flex-col">
-                <div className={`w-36 h-36 rounded-md border border-black justify-center items-center flex flex-col   `}>
-                  <div>
+              <div className="flex w-full justify-center items-center" key={key}>
+                <div
+                  className={`w-[90%] h-96 max-w-[250px] rounded-sm border border-rojoprincipal shadow items-center flex flex-col justify-between`}
+                >
+                  <div className="h-1/2 flex justify-start items-center w-full">
                     <Image
+                      className="w-full p-2"
                       src={producto.images[0]}
                       width={80}
                       height={80}
                       alt="ImageProtada"
                     />
                   </div>
-                  <p className=" font-lato text-center">{producto.name}</p>
+                  <div className="h-1/3 justify-start flex flex-col w-[80%]">
+                    <h5 className="font-lato">{producto.name}</h5>
+                    <button className=" bg-slate-100 mt-10 rounded-sm h-8 border border-slate-200 shadow">
+                    <a href="" className="text-center ">Consultar precio</a>
+                    </button>
+                  </div>
                 </div>
               </div>
-            </a>
           );
         })}
       </div>
