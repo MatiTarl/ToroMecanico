0d52b936a54b304bad2b2ec0b01c232dce339e5e
MatiTarl <Matiasvalentintari123@gmail.com>
2025-02-11 14:03:34 -0300

    Se agegraron signos de exclamacion para la imagen asi se pone la descripcion

--- app/components/ControlDeMandoSection/ControlDeMando.jsx
+++ app/components/ControlDeMandoSection/ControlDeMando.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import Image from 'next/image';
 import "./module.css";
 import ImageAllProduct169 from "@/public/Seccion-ImagenObjetos/TolasLasPiezas-16-9.png";
-import LupaBlack from '@/public/icons/lupa';
+import Exclamacion from '@/public/icons/signoExclamacion';
 
 export default function ControlDeMando() {
   return (
@@ -17,7 +17,27 @@ export default function ControlDeMando() {
           </h1>
         </div>
         <div>
-          <div className='absolute z-50 w-6 h-6'><LupaBlack/></div>
+
+          {/* palos amarillos */}
+          <div className='absolute z-50 w-6 h-6 animate-pulse rounded-full p-1 right-[50%] translate-y-[148px] translate-x-6'><Exclamacion/></div>
+
+          {/* cuadrado gris, panel de control */}
+          <div className='absolute z-50 w-6 h-6 animate-pulse rounded-full p-1 right-[50%] translate-y-[175px] translate-x-20'><Exclamacion/></div>
+
+          {/* Cuadrado con texto Danger */}
+          <div className='absolute z-50 w-6 h-6 animate-pulse rounded-full p-1 right-[50%] -translate-x-7 translate-y-24'><Exclamacion/></div> 
+
+          {/* toro mecanico negro */}
+          <div className='absolute z-50 w-6 h-6 animate-pulse rounded-full p-1 right-[50%] translate-x-16 translate-y-24'><Exclamacion/></div>
+
+          {/* lona verde */}
+          <div className='absolute z-50 w-6 h-6 animate-pulse rounded-full p-1 translate-x-12 translate-y-20'><Exclamacion/></div>
+
+          {/* triangulo azul con maquina de corcobeo */}
+          <div className='absolute z-50 w-6 h-6 animate-pulse rounded-full p-1 right-0 -translate-x-3 translate-y-9'><Exclamacion/></div>
+
+          {/* Turbina abajo derecha */}
+          <div className='absolute z-50 w-6 h-6 animate-pulse rounded-full p-1 right-0 -translate-x-6 translate-y-[115px]'><Exclamacion/></div> 
           <Image
             width={Infinity}
             height={Infinity}
