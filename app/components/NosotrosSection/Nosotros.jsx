78bee7a3a75c14400b739235bde4120ae3e65c3c
MatiTarl <Matiasvalentintari123@gmail.com>
2025-02-25 13:35:28 -0300

    Css para notebook apartado nosotros y section 1

--- app/components/NosotrosSection/Nosotros.jsx
+++ app/components/NosotrosSection/Nosotros.jsx
@@ -5,23 +5,24 @@ import Image from "next/image";
 export default function Nosotros() {
   return (
     <section className="w-full pb-5">
-      <div className="w-full h-40 flex p-1">
-        <div className="w-[50%] flex max-w-[300px] aspect-[2/3] justify-center">
-          <div className=" rounded-md items-start h-[140px] w-1/6 z-0 bg-rojoprincipal opacity-50 absolute justify-start -translate-x-11 translate-y-4 lg:translate-y-[18%] [1920px]:translate-y-[17%] lg:-translate-x-[100%] transition-transform"></div>
+      <div className="w-full h-40 lg:h-full flex p-1 lg:justify-center">
+        <div className="w-[50%] lg:w-auto flex lg:items-center justify-center ">
+          <div className=" rounded-md items-start h-[140px] lg:h-[300px] w-1/6 z-0 bg-rojoprincipal opacity-50 absolute justify-start -translate-x-11 translate-y-4 lg:translate-y-[18%] [1920px]:translate-y-[17%] lg:-translate-x-[13%] transition-transform"></div>
           <Image
             height={Infinity}
             width={Infinity}
             src={ImagePersona}
             alt="ImageAllProduct"
-            className="object-cover rounded-lg max-w-32 min-h-36 z-10"
+            className="aspect-[2/3] object-cover rounded-lg max-w-32 min-h-36 z-10 lg:max-w-64  "
           ></Image>
         </div>
-        <div className="w-[50%] flex flex-col text-xs font-roboto">
-          <h1 className="text-rojoprincipal text-xl lg:text-4xl xl:text-5xl 2xl:text-6xl font-extrabold">
+        <div className="w-[50%] lg:w-[30%] lg:mx-10 lg:h-full flex flex-col text-xs font-roboto">
+          <h1 className="text-rojoprincipal text-xl lg:text-4xl xl:text-5xl 2xl:text-6xl font-extrabold lg:mt-4">
             Nombre ejemplo
           </h1>
+          <br />
           <span className=" md:hidden h-[1px] bg-rojosecundario w-[50%] mb-2"></span>
-          <span className="text-black text-left">
+          <span className="text-black text-left lg:max-w-56">
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias
             quasi nam quam dolorum neque minus ipsam totam enim ut magnam.
           </span>
