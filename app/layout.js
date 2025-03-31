1776fdab2c9f220966cdc50227e05f9a57d099cd
MatiTarl <MatiasTari@outlook.com.ar>
2025-03-31 16:47:32 -0300

    Correcion de metadata 3

--- app/layout.js
+++ app/layout.js
@@ -4,6 +4,10 @@ export const metadata = {
   title: "Juegos Inflables",
   description: "Fábrica de juegos inflables y mecánicos.",
   openGraph: {
+    title: "Juegos Inflables",
+    description: "Fábrica de juegos inflables y mecánicos.",
+    url: "https://texasbull.vercel.app/",
+    siteName: "Juegos inflables",
     image: [
       {
         url: "https://texasbull.vercel.app/favicon.ico",
